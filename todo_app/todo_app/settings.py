c21a7e93a76aada59418c60e3630f0de5688389f
VanV <vainavinair2009558@gmail.com>
2023-04-09 23:08:00 +0530

    added model, set up DB

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'todo_home',
+    'todo_home.apps.TodoHomeConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'todo_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo',
+        'USER':'postgres',
+        'PASSWORD': 'admin',
+        'HOST':'127.0.0.1',
+        'PORT': '5432'
     }
 }
 
--- todo_app/todo_home/models.py
+++ todo_app/todo_home/models.py
@@ -1,3 +1,11 @@
+from datetime import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    decription = models.TextField()
+    due_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE,default=User.objects.first().id)
